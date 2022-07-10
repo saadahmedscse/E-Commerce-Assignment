995682accfcd62bbcb2ae3244fadea1392588318
Saad <saadahmedscse@gmail.com>
2022-07-10 22:43:45 +0600

    Made splash activity full screen

--- app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/splash/SplashActivity.kt
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.saadahmedsoft.e_commerce_assignment.view.splash
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.saadahmedsoft.e_commerce_assignment.R
 import com.saadahmedsoft.e_commerce_assignment.helper.delay
 import com.saadahmedsoft.e_commerce_assignment.view.accountType.AccountTypeActivity
@@ -13,6 +14,9 @@ import com.saadahmedsoft.shortintent.ShortIntent
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         setContentView(R.layout.activity_splash)
 
         delay(750) {
