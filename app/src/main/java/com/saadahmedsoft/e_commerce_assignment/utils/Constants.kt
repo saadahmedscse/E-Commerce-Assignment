afbf8fb78bdcec82926a145fd79a6330ad883b20
Saad <saadahmedscse@gmail.com>
2022-07-10 22:40:51 +0600

    Constant class

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.saadahmedsoft.e_commerce_assignment.utils
+
+class Constants {
+    object App {
+        const val APP_NAME = "E-Commerce Assignment"
+    }
+
+    object Theme {
+        const val THEME_STATE = "THEME_STATE"
+    }
+
+    object Booleans {
+        const val TRUE = true
+        const val FALSE = false
+    }
+
+    object User {
+        const val IS_LOGGED_IN = "IS_LOGGED_IN"
+    }
+}
