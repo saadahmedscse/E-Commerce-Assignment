2176034bc8c5c244618f9d70f282bcb57de48bae
Saad <saadahmedscse@gmail.com>
2022-07-11 01:01:04 +0600

    Implemented base activity

--- app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/dashboard/DashboardActivity.kt
@@ -1,12 +1,19 @@
 package com.saadahmedsoft.e_commerce_assignment.view.dashboard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saadahmedsoft.base.BaseActivity
 import com.saadahmedsoft.e_commerce_assignment.R
+import com.saadahmedsoft.e_commerce_assignment.databinding.ActivityDashboardBinding
+import com.saadahmedsoft.e_commerce_assignment.databinding.AppToolbarBinding
 
-class DashboardActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+class DashboardActivity : BaseActivity<ActivityDashboardBinding>(ActivityDashboardBinding::inflate) {
+
+    override val toolbarBinding: AppToolbarBinding?
+        get() = binding.appToolbar
+
+    override fun onActivityCreate(savedInstanceState: Bundle?) {
+        //
     }
+
+    override fun observeData() {}
 }
