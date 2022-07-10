d8829ebadeb8512a2a9233aa0c4c292ddf710934
Saad <saadahmedscse@gmail.com>
2022-07-10 22:41:33 +0600

    Implemented short intent

--- app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/splash/SplashActivity.kt
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/splash/SplashActivity.kt
@@ -4,11 +4,22 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.saadahmedsoft.e_commerce_assignment.R
+import com.saadahmedsoft.e_commerce_assignment.helper.delay
+import com.saadahmedsoft.e_commerce_assignment.view.accountType.AccountTypeActivity
+import com.saadahmedsoft.shortintent.Anim
+import com.saadahmedsoft.shortintent.ShortIntent
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        delay(750) {
+            ShortIntent.getInstance(this)
+                .addDestination(AccountTypeActivity::class.java)
+                .addTransition(Anim.FADE)
+                .finish(this)
+        }
     }
 }
