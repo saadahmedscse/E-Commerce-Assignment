19b75af6f1d8816ae11c9b8351512f7bbbd9c2ab
Saad <saadahmedscse@gmail.com>
2022-07-11 12:59:18 +0600

    Product adapter for admin functionality implemented

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/dashboard/tabs/adminArea/products/adapter/ProductAdapter.kt
@@ -0,0 +1,28 @@
+package com.saadahmedsoft.e_commerce_assignment.view.dashboard.tabs.adminArea.products.adapter
+
+import android.content.Context
+import com.saadahmedsoft.base.BaseRecyclerAdapter
+import com.saadahmedsoft.base.utils.onClicked
+import com.saadahmedsoft.e_commerce_assignment.R
+import com.saadahmedsoft.e_commerce_assignment.databinding.ItemProductAdminBinding
+import com.saadahmedsoft.e_commerce_assignment.helper.stringToBitmap
+import com.saadahmedsoft.e_commerce_assignment.services.model.Product
+
+class ProductAdapter(val context: Context, private val listener: OnAdminItemActionListener<Product>) : BaseRecyclerAdapter<Product, ItemProductAdminBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.item_product_admin
+
+    override fun onBind(binding: ItemProductAdminBinding, item: Product, position: Int) {
+        binding.productImage.setImageBitmap(stringToBitmap(item.bitmap))
+        binding.productTitle.text = item.name
+        binding.etProductCategory.text = item.category
+
+        binding.btnEdit.onClicked {
+            listener.onEditClickListener(it, item, position)
+        }
+
+        binding.btnDelete.onClicked {
+            listener.onDeleteClickListener(it, item, position)
+        }
+    }
+}
