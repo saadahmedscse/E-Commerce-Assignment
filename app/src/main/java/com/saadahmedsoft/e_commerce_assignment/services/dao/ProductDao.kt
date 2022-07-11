c460a0bdcbdd27c91258dcd75272dc7753b20fd4
Saad <saadahmedscse@gmail.com>
2022-07-11 10:59:40 +0600

    Added Product Dao

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/services/dao/ProductDao.kt
@@ -0,0 +1,21 @@
+package com.saadahmedsoft.e_commerce_assignment.services.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.saadahmedsoft.e_commerce_assignment.services.model.Product
+import com.saadahmedsoft.e_commerce_assignment.utils.Constants.Databse.TABLE_NAME
+
+@Dao
+interface ProductDao {
+    @Query("SELECT * FROM $TABLE_NAME ORDER BY id ASC")
+    fun getProducts(): LiveData<List<Product>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertProduct(product: Product)
+
+    @Update
+    fun updateProduct(product: Product)
+
+    @Query("DELETE FROM $TABLE_NAME WHERE id=:id")
+    fun deleteProduct(id: Int)
+}
