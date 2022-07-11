5d524b15284326c6c3cda192b0fbe40d9e444dc1
Saad <saadahmedscse@gmail.com>
2022-07-11 14:04:11 +0600

    added StaggeredGridLayoutManager

--- app/src/main/java/com/saadahmedsoft/e_commerce_assignment/helper/layoutManager.kt
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/helper/layoutManager.kt
@@ -2,5 +2,7 @@ package com.saadahmedsoft.e_commerce_assignment.helper
 
 import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 fun linearLayoutManager(context: Context) = LinearLayoutManager(context)
+fun staggeredGridLayoutManager() = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
