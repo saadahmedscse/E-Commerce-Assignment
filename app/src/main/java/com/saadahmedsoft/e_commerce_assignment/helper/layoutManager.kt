cb947ae0d60df37dd9ebaffc28113085c845910e
Saad <saadahmedscse@gmail.com>
2022-07-11 12:58:55 +0600

    added layout manager

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/helper/layoutManager.kt
@@ -0,0 +1,6 @@
+package com.saadahmedsoft.e_commerce_assignment.helper
+
+import android.content.Context
+import androidx.recyclerview.widget.LinearLayoutManager
+
+fun linearLayoutManager(context: Context) = LinearLayoutManager(context)
