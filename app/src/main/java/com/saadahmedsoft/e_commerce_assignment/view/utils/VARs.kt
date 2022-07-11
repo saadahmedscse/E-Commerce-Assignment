118adf371bd7efcba38e8664b1ae3e6cd4469361
Saad <saadahmedscse@gmail.com>
2022-07-11 13:31:50 +0600

    Variables for updating

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/utils/VARs.kt
@@ -0,0 +1,12 @@
+package com.saadahmedsoft.e_commerce_assignment.view.utils
+
+class VARs {
+
+    object Product {
+        var IMAGE = ""
+        var NAME = ""
+        var CATEGORY = ""
+        var PRICE = ""
+        var DESCRIPTION = ""
+    }
+}
