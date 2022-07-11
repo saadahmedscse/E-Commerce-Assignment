ed90aab92e1611ce988bebb836194f293b20f5cc
Saad <saadahmedscse@gmail.com>
2022-07-11 12:59:44 +0600

    Product Fragment Functionality implemented

--- app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/dashboard/tabs/adminArea/products/ProductsFragment.kt
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/dashboard/tabs/adminArea/products/ProductsFragment.kt
@@ -1,28 +1,71 @@
 package com.saadahmedsoft.e_commerce_assignment.view.dashboard.tabs.adminArea.products
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.saadahmedsoft.base.BaseFragment
+import com.saadahmedsoft.base.BaseRecyclerAdapter
+import com.saadahmedsoft.base.utils.gone
 import com.saadahmedsoft.base.utils.onClicked
+import com.saadahmedsoft.base.utils.visible
 import com.saadahmedsoft.e_commerce_assignment.R
 import com.saadahmedsoft.e_commerce_assignment.databinding.FragmentProductsBinding
+import com.saadahmedsoft.e_commerce_assignment.helper.linearLayoutManager
 import com.saadahmedsoft.e_commerce_assignment.helper.navigate
+import com.saadahmedsoft.e_commerce_assignment.helper.observe
+import com.saadahmedsoft.e_commerce_assignment.services.model.Product
 import com.saadahmedsoft.e_commerce_assignment.utils.Constants.Booleans.FALSE
+import com.saadahmedsoft.e_commerce_assignment.view.dashboard.tabs.adminArea.products.adapter.ProductAdapter
 
-class ProductsFragment : BaseFragment<FragmentProductsBinding>(FragmentProductsBinding::inflate) {
+class ProductsFragment : BaseFragment<FragmentProductsBinding>(FragmentProductsBinding::inflate), BaseRecyclerAdapter.OnAdminItemActionListener<Product> {
 
     override val title: String
         get() = "Products"
     override val isBackButtonVisible: Boolean
         get() = FALSE
 
+    private val adapter by lazy {
+        ProductAdapter(requireContext(), this)
+    }
+
+    private val TAG ="product_debug"
+
     override fun onFragmentCreate(savedInstanceState: Bundle?) {
+        binding.recyclerView.layoutManager = linearLayoutManager(requireContext())
+
         binding.addBtn.onClicked {
             navigate(it, R.id.product_to_add_product)
         }
         binding.layoutAddProduct.onClicked {
             navigate(it, R.id.product_to_add_product)
         }
+
+        viewModel.getProducts().observe(this) {
+            Log.d(TAG, "observeData: ${it.size}")
+            if (it.isEmpty()) {
+                binding.layoutAddProduct.visible()
+                binding.addBtn.gone()
+                binding.recyclerView.gone()
+            }
+            else {
+                binding.recyclerView.visible()
+                binding.addBtn.visible()
+                binding.layoutAddProduct.gone()
+
+                binding.recyclerView.adapter = adapter
+                adapter.addItems(it)
+            }
+        }
     }
 
     override fun observeData() {}
+
+    override fun onEditClickListener(view: View, item: Product, position: Int) {
+        //go to edit section
+    }
+
+    override fun onDeleteClickListener(view: View, item: Product, position: Int) {
+        viewModel.deleteProduct(item.id!!)
+        shortSnackBar("Product deleted successfully")
+    }
 }
