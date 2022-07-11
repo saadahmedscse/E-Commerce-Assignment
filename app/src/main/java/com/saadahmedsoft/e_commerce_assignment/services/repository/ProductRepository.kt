03fa2828a5867b63f9b91f526b64503fcf51a907
Saad <saadahmedscse@gmail.com>
2022-07-11 10:59:55 +0600

    Added Product Repository

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/services/repository/ProductRepository.kt
@@ -0,0 +1,22 @@
+package com.saadahmedsoft.e_commerce_assignment.services.repository
+
+import androidx.lifecycle.LiveData
+import com.saadahmedsoft.e_commerce_assignment.services.dao.ProductDao
+import com.saadahmedsoft.e_commerce_assignment.services.model.Product
+
+class ProductRepository(private val dao: ProductDao) {
+
+    fun getProducts(): LiveData<List<Product>> = dao.getProducts()
+
+    fun insertProduct(product: Product) {
+        dao.insertProduct(product)
+    }
+
+    fun updateProduct(product: Product) {
+        dao.updateProduct(product)
+    }
+
+    fun deleteProduct(id: Int) {
+        dao.deleteProduct(id)
+    }
+}
