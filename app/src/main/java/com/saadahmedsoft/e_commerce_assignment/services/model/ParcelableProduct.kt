39b2f1ee3d2c21217af2422f58fa2cf4e0983c2f
Saad <saadahmedscse@gmail.com>
2022-07-11 17:16:12 +0600

    Added Parcelable Product

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/services/model/ParcelableProduct.kt
@@ -0,0 +1,9 @@
+package com.saadahmedsoft.e_commerce_assignment.services.model
+
+data class ParcelableProduct(
+    var bitmap: String = "",
+    var name: String = "",
+    var category: String = "",
+    var price: String = "",
+    var description: String = ""
+)
