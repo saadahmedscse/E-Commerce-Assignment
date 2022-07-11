5921d89ec234b11f404987b88a26197e4b7a692e
Saad <saadahmedscse@gmail.com>
2022-07-11 17:18:15 +0600

    Implemented intent for details

--- app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/dashboard/tabs/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/view/dashboard/tabs/favorite/FavoriteFragment.kt
@@ -8,12 +8,19 @@ import com.saadahmedsoft.base.utils.gone
 import com.saadahmedsoft.base.utils.visible
 import com.saadahmedsoft.e_commerce_assignment.databinding.FragmentFavoriteBinding
 import com.saadahmedsoft.e_commerce_assignment.helper.staggeredGridLayoutManager
+import com.saadahmedsoft.e_commerce_assignment.services.model.ParcelableProduct
 import com.saadahmedsoft.e_commerce_assignment.services.model.Product
 import com.saadahmedsoft.e_commerce_assignment.utils.Constants
 import com.saadahmedsoft.e_commerce_assignment.utils.Constants.Booleans.TRUE
+import com.saadahmedsoft.e_commerce_assignment.utils.Constants.Product.KEY_PRODUCT
+import com.saadahmedsoft.e_commerce_assignment.view.dashboard.tabs.details.DetailsActivity
 import com.saadahmedsoft.e_commerce_assignment.view.dashboard.tabs.home.adapter.ProductAdapter
+import com.saadahmedsoft.shortintent.Anim
+import com.saadahmedsoft.shortintent.ShortIntent
+import com.saadahmedsoft.tinydb.TinyDB
 
-class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteBinding::inflate), BaseRecyclerAdapter.OnItemActionListener<Product> {
+class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteBinding::inflate),
+    BaseRecyclerAdapter.OnItemActionListener<Product> {
 
     override val title: String
         get() = "Favorite Products"
@@ -31,8 +38,7 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteB
             if (it.isEmpty()) {
                 binding.layoutNoProduct.visible()
                 binding.recyclerView.gone()
-            }
-            else {
+            } else {
                 binding.recyclerView.visible()
                 binding.layoutNoProduct.gone()
 
@@ -45,7 +51,19 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteB
     override fun observeData() {}
 
     override fun onItemClickListener(view: View, item: Product, position: Int) {
-        //
+        TinyDB.getInstance(requireContext())
+            .putObject(
+                KEY_PRODUCT, ParcelableProduct(
+                    bitmap = item.bitmap,
+                    name = item.name,
+                    category = item.category,
+                    description = item.description
+                )
+            )
+            .apply()
+
+        ShortIntent.getInstance(requireActivity())
+            .addDestination(DetailsActivity::class.java)
     }
 
     override fun onItemLongPressListener(view: View, item: Product, position: Int) {
@@ -53,14 +71,16 @@ class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteB
     }
 
     override fun onFavoriteClickListener(view: View, item: Product, position: Int) {
-        viewModel.updateProduct(Product(
-            id = item.id,
-            bitmap = item.bitmap,
-            name = item.name,
-            category = item.category,
-            price = item.price,
-            description = item.description,
-            isFavorite = if (item.isFavorite) Constants.Booleans.FALSE else TRUE
-        ))
+        viewModel.updateProduct(
+            Product(
+                id = item.id,
+                bitmap = item.bitmap,
+                name = item.name,
+                category = item.category,
+                price = item.price,
+                description = item.description,
+                isFavorite = if (item.isFavorite) Constants.Booleans.FALSE else TRUE
+            )
+        )
     }
 }
