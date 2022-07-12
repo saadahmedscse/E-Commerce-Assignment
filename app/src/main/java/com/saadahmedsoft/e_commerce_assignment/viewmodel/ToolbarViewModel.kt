b52b64fe19d527eb15bf5f03559e05f1cb49349b
Saad <saadahmedscse@gmail.com>
2022-07-13 03:16:33 +0600

    Added toolbar viewModel for toolbar title and back button

---
+++ app/src/main/java/com/saadahmedsoft/e_commerce_assignment/viewmodel/ToolbarViewModel.kt
@@ -0,0 +1,24 @@
+package com.saadahmedsoft.e_commerce_assignment.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolbarViewModel : ViewModel() {
+
+    private val _title = MutableLiveData<String>()
+    val title: LiveData<String>
+        get() = _title
+
+    private val _isBackButtonVisible = MutableLiveData<Boolean>()
+    val isBackButtonVisible: LiveData<Boolean>
+        get() = _isBackButtonVisible
+
+    fun setTitle(title: String) {
+        _title.postValue(title)
+    }
+
+    fun setBackButtonState(state: Boolean) {
+        _isBackButtonVisible.postValue(state)
+    }
+}
