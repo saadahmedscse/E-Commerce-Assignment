e64231e3f640bfd557f66535315b6862bde4b62d
Saad <saadahmedscse@gmail.com>
2022-07-13 05:50:27 +0600

    Removed ParcelableProduct.kt

--- app/src/main/java/com/saadahmedsoft/e_commerce_assignment/services/model/ParcelableProduct.kt
+++
@@ -1,9 +0,0 @@
-package com.saadahmedsoft.e_commerce_assignment.services.model
-
-data class ParcelableProduct(
-    var bitmap: String = "",
-    var name: String = "",
-    var category: String = "",
-    var price: String = "",
-    var description: String = ""
-)
